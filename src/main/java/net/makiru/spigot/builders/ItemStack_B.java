40a8152ce2726c02b7a21e95f56e0c5636dbcb49
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-02-22 23:48:21 +0100

    Create ItemStack_B

---
+++ src/main/java/net/makiru/spigot/builders/ItemStack_B.java
@@ -0,0 +1,373 @@
+package net.makiru.spigot.builders;
+
+import com.google.common.collect.Multimap;
+import org.bukkit.Color;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.material.MaterialData;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+/**
+ * Copyright © LEVASSEUR Wesley
+ *
+ * @author LEVASSEUR Wesley
+ */
+public class ItemStack_B {
+    private ItemStack itemStack;
+
+    public ItemStack_B(@NotNull ItemStack itemStack) {
+        this.itemStack = itemStack;
+    }
+
+    public ItemStack_B(@NotNull Material type) {
+        this(type, 1);
+    }
+
+    public ItemStack_B(@NotNull Material type, int amount) {
+        this(type, amount, (short) 0);
+    }
+
+    public ItemStack_B(@NotNull Material type, int amount, short damage) {
+        this(type, amount, damage, null);
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemStack_B(@NotNull Material type, int amount, short damage, Byte data) {
+        this.itemStack = new ItemStack(type, amount, damage, data);
+    }
+
+    //TODO: ItemStack
+    public ItemStack_B setType(Material type) {
+        this.itemStack.setType(type);
+        return this;
+    }
+
+    public ItemStack_B setAmount(int amount) {
+        this.itemStack.setAmount(amount);
+        return this;
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemStack_B setData(MaterialData materialData) {
+        this.itemStack.setData(materialData);
+        return this;
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemStack_B setDurability(short dur) {
+        this.itemStack.setDurability(dur);
+        return this;
+    }
+
+    @SuppressWarnings("deprecation")
+    public ItemStack_B unbreakable() {
+        this.itemStack.setDurability(Short.MIN_VALUE);
+        return this;
+    }
+
+    public ItemStack_B addEnchantments(Map<Enchantment, Integer> enchantments) {
+        this.itemStack.addEnchantments(enchantments);
+        return this;
+    }
+
+    public ItemStack_B addEnchantment(@NotNull Enchantment ench, int level) {
+        this.itemStack.addEnchantment(ench, level);
+        return this;
+    }
+
+    public ItemStack_B addUnsafeEnchantment(Enchantment ench, int level) {
+        this.itemStack.addUnsafeEnchantment(ench, level);
+        return this;
+    }
+
+    public ItemStack_B addUnsafeEnchantments(Map<Enchantment, Integer> enchantments) {
+        this.itemStack.addUnsafeEnchantments(enchantments);
+        return this;
+    }
+
+    public ItemStack_B removeEnchantment(Enchantment ench) {
+        this.itemStack.removeEnchantment(ench);
+        return this;
+    }
+
+    public boolean hasItemMeta() {
+        return this.itemStack.hasItemMeta();
+    }
+
+    public ItemStack_B setItemMeta(ItemMeta itemMeta) {
+        this.itemStack.setItemMeta(itemMeta);
+        return this;
+    }
+
+    //TODO: ItemMeta
+    public ItemStack_B setDisplayName(String name) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setDisplayName(name);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B setLocalizedName(String name) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setLocalizedName(name);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B setLore(String... lore) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setLore(Arrays.asList(lore));
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B setLore(List<String> lore) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setLore(lore);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeLoreLine(String line) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            List<String> lore = new ArrayList<>(Objects.requireNonNull(itemMeta.getLore()));
+            if (!lore.contains(line)) {
+                return this;
+            }
+            lore.remove(line);
+            itemMeta.setLore(lore);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeLoreLine(int index) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            List<String> lore = new ArrayList<>(Objects.requireNonNull(itemMeta.getLore()));
+            if ((index < 0) || (index > lore.size())) {
+                return this;
+            }
+            lore.remove(index);
+            itemMeta.setLore(lore);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B addLoreLine(String line) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            List<String> lore = new ArrayList<>();
+            if (itemMeta.hasLore()) {
+                lore = new ArrayList<>(Objects.requireNonNull(itemMeta.getLore()));
+            }
+            lore.add(line);
+            itemMeta.setLore(lore);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B addLoreLine(String line, int pos) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            List<String> lore = new ArrayList<>(Objects.requireNonNull(itemMeta.getLore()));
+            lore.set(pos, line);
+            itemMeta.setLore(lore);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+
+    public ItemStack_B setCustomModelData(Integer integer) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setCustomModelData(integer);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B addEnchant(Enchantment enchantment, int level) {
+        this.addEnchant(enchantment, level, true);
+        return this;
+    }
+
+    public ItemStack_B addEnchant(@NotNull Enchantment enchantment, int level, boolean b) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.addEnchant(enchantment, level, b);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeEnchant(@NotNull Enchantment enchantment) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.removeEnchant(enchantment);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B addItemFlags(@NotNull ItemFlag... itemFlags) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.addItemFlags(itemFlags);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeItemFlags(@NotNull ItemFlag... itemFlags) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.removeItemFlags(itemFlags);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B setUnbreakable(boolean b) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setUnbreakable(b);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B addAttributeModifier(@NotNull Attribute attribute, @NotNull AttributeModifier attributeModifier) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.addAttributeModifier(attribute, attributeModifier);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B setAttributeModifiers(@Nullable Multimap<Attribute, AttributeModifier> attributeModifiers) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.setAttributeModifiers(attributeModifiers);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeAttributeModifier(@NotNull Attribute attribute) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.removeAttributeModifier(attribute);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeAttributeModifier(@NotNull EquipmentSlot equipmentSlot) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.removeAttributeModifier(equipmentSlot);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B removeAttributeModifier(@NotNull Attribute attribute, @NotNull AttributeModifier attributeModifier) {
+        ItemMeta itemMeta = this.itemStack.getItemMeta();
+        if (itemMeta != null) {
+            itemMeta.removeAttributeModifier(attribute, attributeModifier);
+            this.itemStack.setItemMeta(itemMeta);
+        }
+        return this;
+    }
+
+    //TODO: SkullMeta
+    @SuppressWarnings("deprecation")
+    public ItemStack_B setSkullOwner(String owner) {
+        SkullMeta skullMeta = (SkullMeta) this.itemStack.getItemMeta();
+        if (skullMeta != null) {
+            skullMeta.setOwner(owner);
+            this.itemStack.setItemMeta(skullMeta);
+        }
+        return this;
+    }
+
+    public ItemStack_B setSkullOwningPlayer(OfflinePlayer owner) {
+        SkullMeta skullMeta = (SkullMeta) this.itemStack.getItemMeta();
+        if (skullMeta != null) {
+            skullMeta.setOwningPlayer(owner);
+            this.itemStack.setItemMeta(skullMeta);
+        }
+        return this;
+    }
+
+    //TODO: LeatherAmorMeta
+    public ItemStack_B setLeatherArmorColor(Color color) {
+        LeatherArmorMeta leatherArmorMeta = (LeatherArmorMeta) this.itemStack.getItemMeta();
+        if (leatherArmorMeta != null) {
+            leatherArmorMeta.setColor(color);
+            this.itemStack.setItemMeta(leatherArmorMeta);
+        }
+        return this;
+    }
+
+
+    //TODO: Other
+    @Deprecated
+    public ItemStack_B setWoolColor(DyeColor dyeColor) {
+        if (!itemStack.getType().name().contains("WOOL")) {
+            return this;
+        }
+        this.itemStack.setDurability(dyeColor.getWoolData());
+        return this;
+    }
+
+    public ItemStack_B addGlow() {
+        this.itemStack.addUnsafeEnchantment(Enchantment.DURABILITY, -1);
+        net.minecraft.server.v1_14_R1.ItemStack nstack = org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack.asNMSCopy(itemStack);
+        net.minecraft.server.v1_14_R1.NBTTagCompound compound = nstack.getTag();
+        if (compound == null) {
+            compound = new net.minecraft.server.v1_14_R1.NBTTagCompound();
+            nstack.setTag(compound);
+            compound = nstack.getTag();
+        }
+        if (!compound.hasKey("HideFlags")) compound.setInt("HideFlags", 1);
+        nstack.setTag(compound);
+        this.itemStack = org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack.asCraftMirror(nstack);
+        return this;
+    }
+
+    public ItemStack_B clone() throws CloneNotSupportedException {
+        return (ItemStack_B) super.clone();
+    }
+
+    public ItemStack toItemStack() {
+        return itemStack;
+    }
+}
