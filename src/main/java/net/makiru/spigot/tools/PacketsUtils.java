295816c633f79ffa4f282f16bc81c431aeaab78c
Levasseur Wesley <mailpro.levasseur@icloud.com>
2020-02-23 17:04:16 +0100

    Create PacketsUtils

---
+++ src/main/java/net/makiru/spigot/tools/PacketsUtils.java
@@ -0,0 +1,16 @@
+package net.makiru.spigot.tools;
+
+import net.minecraft.server.v1_14_R1.ChatMessageType;
+import net.minecraft.server.v1_14_R1.IChatBaseComponent;
+import net.minecraft.server.v1_14_R1.PacketPlayOutChat;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+public class PacketsUtils {
+
+    public static void sendActionBar(Player p, String message) {
+        IChatBaseComponent cbc = IChatBaseComponent.ChatSerializer.a("{\"text\": \"" + message + "\"}");
+        PacketPlayOutChat ppoc = new PacketPlayOutChat(cbc, ChatMessageType.GAME_INFO);
+        ((CraftPlayer) p).getHandle().playerConnection.sendPacket(ppoc);
+    }
+}
